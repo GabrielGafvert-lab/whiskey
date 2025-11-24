f7ff306032d5853047a9ad5d6d934d4927b0f507
gurkilurk-dev <gurkilurk@gmail.com>
2025-11-24 09:40:15 +0000

    Whiskey

---
+++ Whiskey.py
@@ -0,0 +1,13 @@
+f = open("athletes.csv","r")
+temp = f.readline()
+temp = f.readline()
+print(temp)
+matris=[[],[],[],[],[],[],[],[],[]]
+
+while temp != "":
+    temp=temp.split(",")
+    for i in range(4,len(temp)-1):
+        if temp[i]!="":
+            matris[i-4].append((temp[i],temp[0],temp[1],temp[2],temp[3]))
+    temp = f.readline()
+print(matris[0])
